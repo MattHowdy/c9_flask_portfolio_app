edaa85fee0f8b28857f9a7b803948de8b5650026
MattHowdy <34940481+MattHowdy@users.noreply.github.com>
2018-01-09 16:41:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,15 @@ def add_numbers_post():
   	      	return render_template('add_numbers.html', result=str(total))
   	      except ValueError:
   	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
+	  elif request.method == 'POST':
+  	      print(request.form['text'].split())
+  	      total = 1
+  	      try:
+  	      	for str_num in request.form['text'].split():
+  	      		total *= int(str_num)
+  	      	return render_template('add_numbers.html', result=str(total))
+  	      except ValueError:
+  	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
 	
 	
 	
